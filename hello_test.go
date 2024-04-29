6e237682b672052e0c63c7ef8b761d5270dac215
Oliver <12017199+OliverAppDev@users.noreply.github.com>
2024-04-29 13:26:55 +0100

    Create hello_test.go

---
+++ hello_test.go
@@ -0,0 +1,10 @@
+package plfoe_tmp_pod_ide_1_sdk
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, world."
+	if got := Hello(); got != want {
+		t.Errorf("Hello() = %q, want %q", got, want)
+	}
+}
