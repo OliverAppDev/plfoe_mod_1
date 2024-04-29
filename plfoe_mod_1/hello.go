77b16e9a4c15bc1634a3ab0282d350ca23d30e13
oliverappdev <kp@kamilpiskorz.com>
2024-04-29 14:58:48 +0100

    test commit

--- plfoe_mod_1/hello.go
+++ plfoe_mod_1/hello.go
@@ -1,8 +1,12 @@
 package plfoe_mod_1
 
-import "rsc.io/quote"
+import (
+	"fmt"
+	"rsc.io/quote"
+)
 
 func Hello() string {
+	fmt.Println("Hello, world - from sdk remote.")
 	return quote.Hello()
 }
 
